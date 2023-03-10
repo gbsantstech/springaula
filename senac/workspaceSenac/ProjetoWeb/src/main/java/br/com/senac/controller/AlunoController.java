8d3a2cbcca50c56b06cf8e226734ec1745c68183
gbsantstech <36124872021.2N@senacrjedu.local>
2023-03-09 21:21:45 -0300

    first commit

---
+++ senac/workspaceSenac/ProjetoWeb/src/main/java/br/com/senac/controller/AlunoController.java
@@ -0,0 +1,26 @@
+package br.com.senac.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.senac.service.AlunoService;
+
+@Controller
+@RequestMapping("aluno")
+public class AlunoController {
+	
+	@Autowired
+	private AlunoService alunoService;
+	
+	@GetMapping("/listarAlunos")
+	public ModelAndView listaTodosAlunos() {
+		ModelAndView mv = new ModelAndView("aluno/paginaListaAlunos");
+		mv.addObject("alunos", alunoService.buscarTodosAlunos());
+			
+		return mv;
+	}
+
+}
